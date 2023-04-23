31a0e68d5d4cdc6d7d7193d01c3f45eff2fbea16
haofeng0705 <raphael.hao.alive@gmail.com>
2023-04-23 18:36:24 +0800

    feat(register): register functionality, MongoDB, Prisma setup

---
+++ app/libs/prismadb.ts
@@ -0,0 +1,11 @@
+//这么做是为了防止 next hot load 后 prisma 实例的报错
+import { PrismaClient } from "@prisma/client"
+
+declare global {
+  var prisma: PrismaClient | undefined
+}
+
+const client = globalThis.prisma || new PrismaClient()
+if (process.env.NODE_ENV !== "production") globalThis.prisma = client
+
+export default client
