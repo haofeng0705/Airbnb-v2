4214f244e4d4bab9b46cf8c03a397cf9e8ae8e92
haofeng0705 <raphael.hao.alive@gmail.com>
2023-04-25 09:41:30 +0800

    feat(rent modal): counter component

--- app/components/Modals/RentModal.tsx
+++ app/components/Modals/RentModal.tsx
@@ -4,6 +4,7 @@ import { FieldValues, set } from 'react-hook-form'
 import { useMemo, useState } from 'react'
 
 import CategoryInput from '../inputs/CategoryInput'
+import Counter from '../inputs/Counter'
 import CountrySelect from '../inputs/CountrySelect'
 import Heading from '../Heading'
 import Modal from './Modal'
@@ -54,9 +55,14 @@ const RentModal = () => {
   const imageSrc = watch('imageSrc')
 
   // 动态加载, location 改变就要 rerender Map, 效果:当选中某个国家的时候重新渲染 map
-  const Map = useMemo(() => dynamic(() => import('../Map'), {
-    ssr: false
-  }), [location]);
+  const Map = useMemo(
+    () =>
+      dynamic(() => import('../Map'), {
+        ssr: false
+      }),
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    [location]
+  )
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -140,6 +146,37 @@ const RentModal = () => {
           onChange={(value) => setCustomValue('location', value)}
         />
         <Map center={location?.latlng} />
+      </div>
+    )
+  }
+
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading
+          title='Share some basics about your place'
+          subtitle='What amenitis do you have?'
+        />
+        <Counter
+          onChange={(value) => setCustomValue('guestCount', value)}
+          value={guestCount}
+          title='Guests'
+          subtitle='How many guests do you allow?'
+        />
+        <hr />
+        <Counter
+          onChange={(value) => setCustomValue('roomCount', value)}
+          value={roomCount}
+          title='Rooms'
+          subtitle='How many rooms do you have?'
+        />
+        <hr />
+        <Counter
+          onChange={(value) => setCustomValue('bathroomCount', value)}
+          value={bathroomCount}
+          title='Bathrooms'
+          subtitle='How many bathrooms do you have?'
+        />
       </div>
     )
   }
