1be8dfd5e28150cc15e0b03d4837726946eb44dd
haofeng0705 <raphael.hao.alive@gmail.com>
2023-04-27 13:55:20 +0800

    feat: filter menu

--- app/actions/getListings.ts
+++ app/actions/getListings.ts
@@ -1,32 +1,96 @@
-import { SafeListing } from './../types/index'
 import prisma from '@/app/libs/prismadb'
 
 export interface IListingsParams {
-  userId?: string;
-  guestCount?: number;
-  roomCount?: number;
-  bathroomCount?: number;
-  startDate?: string;
-  endDate?: string;
-  locationValue?: string;
-  category?: string;
+  userId?: string
+  guestCount?: number
+  roomCount?: number
+  bathroomCount?: number
+  startDate?: string
+  endDate?: string
+  locationValue?: string
+  category?: string
 }
 
-export default async function getListings(
-  params: IListingsParams
-) {
+export default async function getListings(params: IListingsParams) {
   try {
+    const {
+      userId,
+      roomCount,
+      guestCount,
+      bathroomCount,
+      locationValue,
+      startDate,
+      endDate,
+      category
+    } = params
+
+    let query: any = {}
+
+    if (userId) {
+      query.userId = userId
+    }
+
+    if (category) {
+      query.category = category
+    }
+
+    if (roomCount) {
+      query.roomCount = {
+        // greater than or equal to, + converts string to number
+        gte: +roomCount
+      }
+    }
+
+    if (guestCount) {
+      query.guestCount = {
+        gte: +guestCount
+      }
+    }
+
+    if (bathroomCount) {
+      query.bathroomCount = {
+        gte: +bathroomCount
+      }
+    }
+
+    if (locationValue) {
+      query.locationValue = locationValue
+    }
+    // if start date and end date are provided, check if the listing is available
+    if (startDate && endDate) {
+      query.NOT = {
+        reservations: {
+          some: {
+            OR: [
+              // 对起始日期的约束
+              {
+                endDate: { gte: startDate },
+                startDate: { lte: startDate }
+              },
+              // 对结束日期的约束
+              {
+                startDate: { lte: endDate },
+                endDate: { gte: endDate }
+              }
+            ]
+          }
+        }
+      }
+    }
+
     const listings = await prisma.listing.findMany({
+      where: query,
       orderBy: {
         createdAt: 'desc'
       }
     })
+
     const safeListings = listings.map((listing) => ({
       ...listing,
-      createdAt: listing.createdAt.toISOString(),
-    }));
+      createdAt: listing.createdAt.toISOString()
+    }))
 
-    return safeListings;
+    return safeListings
   } catch (error: any) {
     throw new Error(error)
   }
