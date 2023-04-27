eba0386ee143c2236a5fe5f2265d4427564a40d9
haofeng0705 <raphael.hao.alive@gmail.com>
2023-04-27 14:06:55 +0800

    feat: show filter in the search bar

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,14 +1,61 @@
 'use client'
 
 import { BiSearch } from 'react-icons/bi'
+import { differenceInDays } from 'date-fns'
+import useCountries from '@/app/hooks/useCountries'
+import { useMemo } from 'react'
 import useSearchModal from '@/app/hooks/useSearchModal'
+import { useSearchParams } from 'next/navigation'
+
 const Search = () => {
   const searchModal = useSearchModal()
+  const params = useSearchParams()
+  const { getByValue } = useCountries()
+
+  const locationValue = params?.get('locationValue')
+  const startDate = params?.get('startDate')
+  const endDate = params?.get('endDate')
+  const guestCount = params?.get('guestCount')
+
+  const locationLabel = useMemo(() => {
+    if (locationValue) {
+      return getByValue(locationValue as string)?.label
+    }
+
+    return 'Anywhere'
+  }, [locationValue, getByValue])
+
+  const durationLabel = useMemo(() => {
+    if (startDate && endDate) {
+      const start = new Date(startDate as string)
+      const end = new Date(endDate as string)
+      let diff = differenceInDays(end, start)
+
+      if (diff === 0) {
+        diff = 1
+      }
+
+      return `${diff} Days`
+    }
+
+    return 'Any Week'
+  }, [startDate, endDate])
+
+  const guestLabel = useMemo(() => {
+    if (guestCount) {
+      return `${guestCount} Guests`
+    }
+
+    return 'Add Guests'
+  }, [guestCount])
+
   return (
-    <div onClick={searchModal.onOpen}
-      className='border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer'>
+    <div
+      onClick={searchModal.onOpen}
+      className='border-[1px] w-full md:w-auto py-2 rounded-full shadow-sm hover:shadow-md transition cursor-pointer'
+    >
       <div className='flex flex-row items-center justify-between'>
-        <div className='text-sm font-semibold px-6'>Anywhere</div>
+        <div className='text-sm font-semibold px-6'>{locationLabel}</div>
         <div
           className='hidden sm:block
             text-sm
@@ -18,7 +65,7 @@ const Search = () => {
             flex-1
             text-center'
         >
-          Any Week
+          {durationLabel}
         </div>
         <div
           className='text-sm
@@ -30,7 +77,7 @@ const Search = () => {
             items-center
             gap-3'
         >
-          <div className='hidden sm:block'>add guest</div>
+          <div className='hidden sm:block'>{guestLabel}</div>
           <div
             className='
               p-2
